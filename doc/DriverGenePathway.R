5a5fd4652e2f54c0e727b6f095532926da22ecc6
xiaolu xu <lu.xu@lnnu.edu.cn>
2023-03-30 10:39:55 +0800

    add doc

---
+++ doc/DriverGenePathway.R
@@ -0,0 +1,18 @@
+## -----------------------------------------------------------------------------
+library(DriverGenePathway)
+data(SampleMutationMaf)
+data(SampleCoverage)
+data(SampleDict)
+data(SampleCovariate)
+
+head(M)
+head(C)
+head(dict)
+head(V)
+
+
+## -----------------------------------------------------------------------------
+library(DriverGenePathway)
+data(SampleMutationMatrix)
+head.matrix(mutation_matrix)
+
